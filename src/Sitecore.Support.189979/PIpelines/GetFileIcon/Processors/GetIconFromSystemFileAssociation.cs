67864a359634fca1237ff5a4b8cd38208e29a4ce
DK\DTAT <dtat@sitecore.net>
2017-10-23 13:05:03 +0300

    Initial code

---
+++ src/Sitecore.Support.189979/PIpelines/GetFileIcon/Processors/GetIconFromSystemFileAssociation.cs
@@ -0,0 +1,31 @@
+﻿namespace Sitecore.Support.Pipelines.GetFIleIcon.Processors
+{
+    using Sitecore.Diagnostics;
+    using Sitecore.IO;
+    using Sitecore.Pipelines.GetFileIcon;
+    using System;
+
+    public class GetIconFromSystemFileAssociation : Sitecore.Pipelines.GetFileIcon.Processors.GetIconFromSystemFileAssociation
+    {
+        public override void Process(GetFileIconPipelineArgs args)
+        {
+            if (!this.IsArgumentsObjectHandled(args))
+            {
+                string shellFileIcon = null;
+                try
+                {
+                    shellFileIcon = this.GetShellFileIcon(args);
+                }
+                catch (Exception)
+                {
+                    Log.SingleWarn("Icon could not be obtained through WinAPI.", typeof(GetIconFromSystemFileAssociation));
+                }
+                if (!string.IsNullOrEmpty(shellFileIcon))
+                {
+                    args.RelativeIconPath = FileUtil.UnmapPath(shellFileIcon);
+                    args.AbortPipeline();
+                }
+            }
+        }
+    }
+}
